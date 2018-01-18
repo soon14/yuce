dca7ce6bbb89c5e3e5b128fe2c40131f6042f40f
luohuanjun <luohuanjun@dingfu.com>
2018-01-18 19:54:11 +0800

    first

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -59,7 +59,10 @@ class HomeController extends Controller
         $user_data['problem_text'] = $data ['problem_text'];
         $user_data['problem_type'] = $data['problem_type'];
         $user_data['ce_sn'] = $sn;
+
         $result = ['s_gua'=>$s_gua,'b_gua'=>$b_gua,'dongyao'=>$dongyao,'user_data'=>$user_data];
+        $result  = $result + $this->tiyong($result);
+
         //记录日志
         $logs['fid'] = $fid;
         $logs['uid'] = $uid;
@@ -67,6 +70,89 @@ class HomeController extends Controller
         DB::table('cz_result')->insert($logs);
 
         return redirect("/get_result?csn=$sn");
+    }
+    private function tiyong($result=[]){
+        $s_gua = $result['s_gua'];
+        $b_gua = $result['b_gua'];
+        $dongyao = $result['dongyao'];
+        $table_e_gua = 'cz_e_gua';
+        $res = [];
+        if($dongyao->position>3){
+            //体卦为下卦
+            $t_gua = DB::table($table_e_gua)->where('id',$s_gua->down_id)->first();
+            $t_gua->tiyong = '体';
+            //用卦
+            $y_gua = DB::table($table_e_gua)->where('id',$s_gua->up_id)->first();
+            $y_gua->tiyong = '用';
+        }else{
+            //体卦为上卦
+            $t_gua = DB::table($table_e_gua)->where('id',$s_gua->up_id)->first();
+            $t_gua->tiyong = '体';
+            //用卦
+            $y_gua = DB::table($table_e_gua)->where('id',$s_gua->down_id)->first();
+            $y_gua->tiyong = '用';
+        }
+        //断生克比和
+        $wuxing = $this->wuxing($t_gua,$y_gua);
+        $ty_str = $wuxing['zhu']->tiyong.$wuxing['guanxi'].$wuxing['bing']->tiyong;
+        $res['tiyong'] = $wuxing;
+        $res['duanyan'] = $this->duanyan($ty_str);
+        return $res;
+    }
+    private function duanyan($str=''){
+        $list = [
+            '体比和用'=>['type'=>'小吉','text'=>'吉，所求事情终会如愿，一切顺利'],
+            '体生用'=>['type'=>'小凶','text'=>'所测事情困难重重，很难成功'],
+            '体克用'=>['type'=>'小吉','text'=>'所测之事能够成功，只是需要一点时间'],
+            '用克体'=>['type'=>'大凶','text'=>'所测之事非但不能成功，还对自己有所损失或者伤害'],
+            '用生体'=>['type'=>'大吉','text'=>'所测之事必然成功，很特别顺心如意'],
+        ];
+         if(isset($list[$str])){
+             return $list[$str];
+         }
+    }
+
+    private function wuxing($a,$b){
+
+        $wuxing = [22=>'金',13=>'木',36=>'水',33=>'火',7=>'土'];
+
+        $a_index = array_search($a->attribute,$wuxing);
+        $b_index = array_search($b->attribute,$wuxing);
+
+        $res = ['zhu'=>'','guanxi'=>'','bing'=>''];
+
+        if($a_index == $b_index){
+            $res = ['zhu'=>$a,'guanxi'=>'比和','bing'=>$b];
+            return $res;
+        }
+        $ab_index_sum = $a_index+$b_index;
+
+        $sheng = [
+            58=>['zhu'=>'金','guanxi'=>'生','bing'=>'水'],
+            49=>['zhu'=>'水','guanxi'=>'生','bing'=>'木'],
+            46=>['zhu'=>'木','guanxi'=>'生','bing'=>'火'],
+            40=>['zhu'=>'火','guanxi'=>'生','bing'=>'土'],
+            29=>['zhu'=>'土','guanxi'=>'生','bing'=>'金'],
+            35=>['zhu'=>'金','guanxi'=>'克','bing'=>'木'],
+            20=>['zhu'=>'木','guanxi'=>'克','bing'=>'土'],
+            43=>['zhu'=>'土','guanxi'=>'克','bing'=>'水'],
+            69=>['zhu'=>'水','guanxi'=>'克','bing'=>'火'],
+            55=>['zhu'=>'火','guanxi'=>'克','bing'=>'金']
+        ];
+        if(isset($sheng[$ab_index_sum])){
+            $gx = $sheng[$ab_index_sum];
+            if($gx['zhu'] == $a->attribute){
+                $res['zhu'] = $a;
+                $res['guanxi'] = $gx['guanxi'];
+                $res['bing'] = $b;
+            }elseif ($gx['zhu'] == $b->attribute){
+                $res['zhu'] = $b;
+                $res['guanxi'] = $gx['guanxi'];
+                $res['bing'] = $a;
+            }
+        }
+        return $res;
+
     }
     public function get_result(Request $request){
         $sn = $request->get('csn');
@@ -88,11 +174,7 @@ class HomeController extends Controller
         $Sizhu = new Sizhu();
         $result->user_data->sizhu = $Sizhu->getSizhu($result->user_data->cesuan_time);
 
-        if(Common::isMobile()){
-            $act_name='qr_result';
-        }else{
-            $act_name='result';
-        }
+        $act_name='result';
         return view('meihua.'.$act_name,['result'=>$result]);
     }
     private   function get_problem_type(){
--- resources/views/meihua/result.blade.php
+++ resources/views/meihua/result.blade.php
@@ -1,47 +1,91 @@
 @extends('layouts.main')
 @section('content')
-<div class="jumbotron">
-	<form id="form1" class="form-horizontal" action="" method="post">
+	<div class="jumbotron">
 		<h2 class="text-center">测算结果</h2>
-		<div class="shuoming">
-			<dl class="dl-horizontal">
-				<dt>测算号</dt>
-				<dd>{{$result->user_data->ce_sn}}</dd>
-				<dt>测算日期</dt>
-				<dd>{{$result->user_data->cesuan_time}} {{$result->user_data->shichen}} 时</dd>
-				<dt>测算四柱</dt>
-				<dd>{{$result->user_data->sizhu['nianzhu']}} {{$result->user_data->sizhu['yuezhu']}} {{$result->user_data->sizhu['rizhu']}} {{$result->user_data->sizhu['shizhu']}}</dd>
-				<dt>所问事项</dt>
-				<dd>{{$result->user_data->problem_type}}</dd>
-				<dt>事情简述</dt>
-				<dd>{{$result->user_data->problem_text}}</dd>
-			</dl>
-			<hr>
-			<div class="result">
-				<h4>卦象</h4>
-				<div class="col-xs-12">
-					<div class="col-xs-6">本卦：<strong>{{$result->s_gua->fullname}}</strong><img src="/data/thumb/{{$result->s_gua->name}}.png" class="img-thumbnail"></div>
-					{{--
-                                <div class="gua_xiang">互卦：<strong>{$result.h_gua.fullname}</strong><img src="/data/thumb/{$result.h_gua.name}.png" class="img-thumbnail"></div>
-                    --}}
-					<div class="col-xs-6">变卦：<strong>{{$result->b_gua->fullname}}</strong><img src="/data/thumb/{{$result->b_gua->name}}.png" class="img-thumbnail"></div>
+		<hr>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="row">
+					<p class="col-sm-2"><strong>测算号</strong></p>
+					<div class="col-sm-10">
+						<p>{{$result->user_data->ce_sn}}</p>
+					</div>
 				</div>
-				<h4>动爻</h4>
-				<p>{{$result->dongyao->name}}</p>
-				<h4>爻辞</h4>
-				<p>{{$result->dongyao->yaoci}}</p>
-				{{--<p>{{$result->dongyao->xiangci}} </p>--}}
-				{{--<h4>白话文解释</h4>
-                <p>{{$result->dongyao->yaoci_jieshi}}</p>
-                <p>{{$result->dongyao->xiangci_jieshi}}</p>--}}
-				<h4>邵康节解卦</h4>
-				<p>{{$result->dongyao->shaoci}}</p>
-				<hr>
-				<div class="alert alert-danger">
-					<strong>善意提醒：</strong>如果您不懂周易，切忌不要按卦辞、爻辞的字面意思理解。
+				<div class="row">
+					<p  class="col-sm-2"><strong>测算日期</strong></p>
+					<div class="col-sm-10">
+						<p>{{$result->user_data->cesuan_time}} {{$result->user_data->shichen}} 时</p>
+					</div>
+				</div>
+				<div class="row">
+					<p class="col-sm-2"><strong>测算四柱</strong></p>
+					<div class="col-sm-10">
+						<p>{{$result->user_data->sizhu['nianzhu']}} {{$result->user_data->sizhu['yuezhu']}} {{$result->user_data->sizhu['rizhu']}} {{$result->user_data->sizhu['shizhu']}}</p>
+					</div>
+				</div>
+				<div class="row">
+					<p  class="col-sm-2"><strong>所问事项</strong></p>
+					<div class="col-sm-10">
+						<p>{{$result->user_data->problem_type}}</p>
+					</div>
+				</div>
+				<div class="row">
+					<p  class="col-sm-2"><strong>事情简述</strong></p>
+					<div class="col-sm-10">
+						<p>{{$result->user_data->problem_text}}</p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</form>
-</div>
+		<hr>
+		<div class="row">
+			<div class="col-xs-12">
+				{{--<dl class="dl-horizontal">
+                    <dt>测算号</dt>
+                    <dd>{{$result->user_data->ce_sn}}</dd>
+                    <dt>测算日期</dt>
+                    <dd>{{$result->user_data->cesuan_time}} {{$result->user_data->shichen}} 时</dd>
+                    <dt>测算四柱</dt>
+                    <dd>{{$result->user_data->sizhu['nianzhu']}} {{$result->user_data->sizhu['yuezhu']}} {{$result->user_data->sizhu['rizhu']}} {{$result->user_data->sizhu['shizhu']}}</dd>
+                    <dt>所问事项</dt>
+                    <dd>{{$result->user_data->problem_type}}</dd>
+                    <dt>事情简述</dt>
+                    <dd>{{$result->user_data->problem_text}}</dd>
+                </dl>
+                <hr>--}}
+				<div class="result">
+					<h4>卦象</h4>
+
+					<div class="col-sm-12">
+						<div class="col-sm-6">本卦：<strong>{{$result->s_gua->fullname}}</strong><img src="/data/thumb/{{$result->s_gua->name}}.png" class="img-thumbnail"></div>
+						{{--
+                                    <div class="gua_xiang">互卦：<strong>{$result.h_gua.fullname}</strong><img src="/data/thumb/{$result.h_gua.name}.png" class="img-thumbnail"></div>
+                        --}}
+						<div class="col-sm-6">变卦：<strong>{{$result->b_gua->fullname}}</strong><img src="/data/thumb/{{$result->b_gua->name}}.png" class="img-thumbnail"></div>
+					</div>
+					<hr>
+					<h4>动爻</h4>
+					<p>{{$result->dongyao->name}}</p>
+					<h4>爻辞</h4>
+					<p>{{$result->dongyao->yaoci}}</p>
+					{{--<p>{{$result->dongyao->xiangci}} </p>--}}
+					{{--<h4>白话文解释</h4>
+                    <p>{{$result->dongyao->yaoci_jieshi}}</p>
+                    <p>{{$result->dongyao->xiangci_jieshi}}</p>--}}
+					<h4>邵康节解卦</h4>
+					<p>{{$result->dongyao->shaoci}}</p>
+					<hr>
+					<h4>智能断卦</h4>
+					<p>{{$result->tiyong->zhu->name}} 为 {{$result->tiyong->zhu->tiyong}} , {{$result->tiyong->bing->name}} 为 {{$result->tiyong->bing->tiyong}}</p>
+					<p>{{$result->tiyong->zhu->name}} {{$result->tiyong->zhu->attribute}} <span class="text-success">{{$result->tiyong->guanxi}}</span> {{$result->tiyong->bing->name}} {{$result->tiyong->bing->attribute}}</p>
+					<p> <strong
+								class="@if($result->duanyan->type = '吉') text-success
+						   @elseif($result->duanyan->type = '小吉') text-primary
+						   @elseif($result->duanyan->type = '小凶') text-warning
+                           @elseif($result->duanyan->type = '大凶') @else text-danger @endif"
+						>{{$result->duanyan->type}}</strong> <span class="">{{$result->duanyan->text}}</span></p>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection

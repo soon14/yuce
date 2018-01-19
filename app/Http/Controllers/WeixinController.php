f2fef1fc25aeb1cb52df340cdfed529e71385871
luohuanjun <luohuanjun@dingfu.com>
2018-01-19 19:11:36 +0800

    first

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -33,7 +33,7 @@ class WeixinController extends Controller
         $app->server->push(function ($message)  {
             $url = env('APP_URL');
             if($message['MsgType'] == 'text'){
-                if(preg_match('/^我要/',$message['Content'])){
+                if(preg_match('/^预测/',$message['Content'])){
                     $data['uid'] = $message['FromUserName'];
                     $data['problem_type'] = '杂事';
                     $data['problem_text'] = $message['Content'];
@@ -43,7 +43,7 @@ class WeixinController extends Controller
                     return "查看结果，戳此链接：$url";
                 }
             }
-            return "您好！欢迎关注易学古今,我还会算卦哦,回复：我要+你要测的事情简述，即可起卦，或者直接戳此链接：".$url;
+            return "您好！欢迎关注易学古今,我还会算卦哦,回复：预测+你要测的事情简述，即可起卦，或者直接戳此链接：".$url;
         });
         $response = $app->server->serve();
         // 将响应输出

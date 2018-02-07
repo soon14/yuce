cce2946d6918b90555d131ef1bd47b9e3fbf9d46
luohuanjun <luohuanjun@dingfu.com>
2018-02-07 16:28:33 +0800

    first

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Tell the browser to be responsive to screen width -->
+    <meta name="description" content="九宫智能预测，以梅花易数为基础，使用人工智能算法智能算卦." />
+    <meta name="keywords" content="预测,算卦,智能预测,人工智能预测,疑难解惑" />
+
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">

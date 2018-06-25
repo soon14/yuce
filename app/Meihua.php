9c432307858640c3f220f3d37a6711b0d26e749a
luohuanjun <luohuanjun@dingfu.com>
2018-06-25 16:00:43 +0800

    first

--- app/Meihua.php
+++ app/Meihua.php
@@ -37,6 +37,8 @@ class Meihua extends Model
         foreach ($user_words as $word){
             $user_word_num += (int)($text->find($word));
         }
+        var_dump($user_str,$user_words,$user_word_num);
+        exit;
         $time_num = date('Y')+date('m')+date('d');
         $data ['t1_len'] = $time_num;
         $data ['t2_len'] = $user_word_num;
